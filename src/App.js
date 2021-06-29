51191019301156abb1c1ed17adefc862ff13998c
Yankov Vlad <vladankov996@gmail.com>
2021-06-29 19:07:19 +0300

    feat: add s1 solution

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import React from 'react'
+import { Tap } from './Tap.js'
+
+export const App = () => {
+
+  let inConsole = () => {
+    console.log('This is first button')
+  }
+  let inAlert = () => {
+    alert('This is second button')
+  }
+  let inConsoleWithArg = (element) => {
+    console.log(element)
+  }
 
 
-export default App;
+  return (
+    <div className="wrap">
+      <Tap label="Console button" onClick={inConsole}/>
+      <Tap label="Alert button" onClick={inAlert}/>
+      <Tap label ="Label to console" onClick={() => inConsoleWithArg('This is third button')}/>
+    </div>
+    
+  )
+}
+
+
