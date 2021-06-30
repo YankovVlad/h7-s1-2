a9b9e85c5235e0c8b1046137551e2c7c0746fad1
Yankov Vlad <vladankov996@gmail.com>
2021-06-30 16:57:46 +0300

    feat: add solution s2

---
+++ src/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from './Button.js'
+
+class Counter extends React.Component {
+    state = {
+        value: 0
+    }
+    onClickPlus = () => {
+        this.setState({value: this.state.value + 1})
+    }
+    onClickMinus = () => {
+        this.setState({value: this.state.value - 1})
+    }
+
+
+    render () {
+        return (
+            <div className="wrap">
+                <Button label="Plus" onClick={this.onClickPlus}/>
+                {this.state.value}
+                <Button label="Minus" onClick={this.onClickMinus}/>
+            </div>
+        )
+    }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App.js';
+import Counter from './Counter.js'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
